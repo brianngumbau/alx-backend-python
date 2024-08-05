3350312f54a114f292524d7e1ff2089186f00197
brianngumbau <brianngumbau9075@gmail.com>
2024-08-05 10:45:03 -0700

    task 2:runtime function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with the specified max_delay
     and returns the list of all the delays (float values).
